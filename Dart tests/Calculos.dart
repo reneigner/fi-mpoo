320fa075012481b40e44de30c60ed03c8c77970a
reneigner <ren.ico.0930@gmail.com>
2023-08-07 17:58:12 +0000

    Ejemplos con Dart

---
+++ Dart tests/Calculos.dart
@@ -0,0 +1,16 @@
+void main() {
+    int num1 = 10;
+    int num2 = 3;
+
+    // Calculos
+    int sum = num1 + num2;
+    int res = num1 - num2;
+    int mul = num1 * num2;
+    double div = num1 / num2;
+
+    // Mostrando resultados
+    print("La suma es $sum");
+    print("La resta es $res");
+    print("La multiplicacion es $mul");
+    print("La division es $div");
+}
---
+++ Dart tests/Hello.dart
@@ -0,0 +1,5 @@
+void main() {
+    var nombre = "Rene";
+    var apellido = "Davila";
+    print("El nombre completo es $nombre $apellido");
+}
