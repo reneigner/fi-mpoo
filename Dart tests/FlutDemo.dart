1634d1a35269fb1a1c4d43a2d895517662c327a8
reneigner <ren.ico.0930@gmail.com>
2023-08-14 18:44:10 +0000

    Demo de Flutter

---
+++ Dart tests/FlutDemo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text('Incrementa'),
+        ),
+        const SizedBox(width: 16),
+        Text('Contador: $_counter'),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
+    ),
+  );
+}
